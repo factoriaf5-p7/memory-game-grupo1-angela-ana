7a1725f1e3da78635285cfd72e142515ff71a289
Ana Pozo <the.analejandra@gmail.com>
2023-08-21 14:39:20 +0200

    #2 resources created

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ThemesModule } from './themes/themes.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ThemesModule],
   controllers: [AppController],
   providers: [AppService],
 })
