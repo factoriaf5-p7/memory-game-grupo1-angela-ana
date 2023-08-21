ec18750ff41fc46a29305ada0d9ae0414b046d03
Angela-GM <angelagm0112@gmail.com>
2023-08-21 11:53:34 +0200

    #2 Front-end Scaffolding done

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  // Añadimos soporte al navegador
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/test/setup.ts'],
+    css: false,
+  },
+  resolve: {
+    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
+  }
+});
