fc3685f0e82aee358fe4f9d8fca30fcc36f1cf8c
Ana Pozo <the.analejandra@gmail.com>
2023-08-25 11:31:59 +0200

    fixed front

--- frontend/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import { RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
-import { Routes } from "@/routes";
-
-const router = createBrowserRouter(createRoutesFromElements(Routes))
-const App = () => <RouterProvider router={router} />;
-
-export default App
--- frontend/src/services/lib/themes.ts
+++
@@ -1,5 +0,0 @@
-import axiosClient from "../apiClient";
-
-export function getThemes() {
-    return axiosClient.get('/theme')
-}
