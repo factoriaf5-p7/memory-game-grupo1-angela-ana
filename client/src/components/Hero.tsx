46fc79b8b7af036059475d32b4f8fe5d0e9a5ec0
Angela-GM <angelagm0112@gmail.com>
2023-08-28 15:05:44 +0200

    update

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'react-router-dom'
 import '../assets/styles/index.css'
 
 export const Hero = () => {
   return (
     <div className='hero'>
-      <img src="https://res.cloudinary.com/drjyg98uv/image/upload/v1693217566/memory-game/kwtljauocd9jt87c8rr5.jpg" />
+     
+      <Link to='/game'> <img src="https://res.cloudinary.com/drjyg98uv/image/upload/v1693217566/memory-game/kwtljauocd9jt87c8rr5.jpg" /></Link>
 
     </div>
   )
